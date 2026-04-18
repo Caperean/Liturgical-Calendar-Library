374dd9859281d9aa8b21c2c451b8a3bcbe327db7
Caperean <sec.guild.deeeep@gmail.com>
2026-04-18 23:01:33 +0200

    Implement rank_for_fixed_feast function
    Added a function to determine the liturgical rank for fixed feasts.

--- src/calendar/fixed_feasts.rs
+++ src/calendar/fixed_feasts.rs
@@ -2,6 +2,8 @@
 
 use chrono::NaiveDate;
 
+use crate::calendar::fixed_feasts::FixedFeast;
+use crate::calendar::rules::LiturgicalRank;
 
 #[derive(Debug, Clone)]
 pub enum FixedFeast {
@@ -30,5 +32,20 @@ pub fn get_fixed_feast(date: NaiveDate) -> Option<FixedFeast> {
         (12, 8)  => Some(FixedFeast::ImmaculateConception),
         (12, 25) => Some(FixedFeast::Christmas),
         _ => None,
+    }
+}
+
+pub fn rank_for_fixed_feast(feast: &FixedFeast) -> LiturgicalRank {
+    match feast {
+        FixedFeast::Christmas => LiturgicalRank::Solemnity,
+        FixedFeast::MaryMotherOfGod => LiturgicalRank::Solemnity,
+        FixedFeast::Epiphany => LiturgicalRank::Solemnity,
+        FixedFeast::PresentationOfTheLord => LiturgicalRank::Feast,
+        FixedFeast::Annunciation => LiturgicalRank::Solemnity,
+        FixedFeast::NativityOfJohnTheBaptist => LiturgicalRank::Solemnity,
+        FixedFeast::PeterAndPaul => LiturgicalRank::Solemnity,
+        FixedFeast::Assumption => LiturgicalRank::Solemnity,
+        FixedFeast::AllSaints => LiturgicalRank::Solemnity,
+        FixedFeast::ImmaculateConception => LiturgicalRank::Solemnity,
     }
 }
