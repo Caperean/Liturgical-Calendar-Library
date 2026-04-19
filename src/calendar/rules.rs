2e8ce2a0773c713ce8b2c9c1f5182cb124d81c65
Caperean <sec.guild.deeeep@gmail.com>
2026-04-19 16:55:01 +0200

    Add Sunday rank and priority method to LiturgicalRank

--- src/calendar/rules.rs
+++ src/calendar/rules.rs
@@ -12,12 +12,24 @@ pub enum LiturgicalSeason {
 #[derive(Debug, Clone, PartialEq, Eq, Serialize)]
 pub enum LiturgicalRank {
     Solemnity,
+    Sunday
     Feast,
     Memorial,
     OptionalMemorial,
     Feria,
 }
-
+impl LiturgicalRank {
+    pub fn priority(&self) -> u8 {
+        match self {
+            LiturgicalRank::Solemnity => 6,
+            LiturgicalRank::Sunday => 5,
+            LiturgicalRank::Feast => 4,
+            LiturgicalRank::Memorial => 3,
+            LiturgicalRank::OptionalMemorial => 2,
+            LiturgicalRank::Feria => 1,
+        }
+    }
+}
 #[derive(Debug, Clone, PartialEq, Eq, Serialize)]
 pub enum LiturgicalColor {
     White,
